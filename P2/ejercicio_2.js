ada80f4cdf1fab5c0a1c89e624e869b2cb68fdc3
David <davidquinterocentro@hotmail.com>
2022-06-18 17:13:12 -0500

    parte2

---
+++ P2/ejercicio_2.js
@@ -0,0 +1,6 @@
+const miLista = ["David", 22, true, new Date(1999, 7, 28)]
+const libro = {
+    titulo:"cartas de amor a los muertos", 
+    autor:"Ava Delaira", 
+    fecha: new Date(2014, 3, 1)
+}
